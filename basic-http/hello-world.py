03455ee560afff529db5c33132b2175c20532ed0
Tom Busby <tom@flyfreeweb.co.uk>
2016-05-18 23:51:31 +0200

    Initial commit: basic http service

---
+++ basic-http/hello-world.py
@@ -0,0 +1,13 @@
+import json
+from nameko.web.handlers import http
+
+class HttpService(object):
+    name = "http_service"
+
+    @http('GET', '/get/<int:value>')
+    def get_method(self, request, value):
+        return json.dumps({'value': value})
+
+    @http('POST', '/post')
+    def do_post(self, request):
+        return "received: {}".format(request.get_data(as_text=True))
